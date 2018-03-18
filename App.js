300a738223eb51ceefc0a54d65ea845b1e9be920
wangqiang16 <wangqiang16@lenovo.com>
2018-03-18 10:56:59 +0800

    修改app.js  只是添加了几个文字
    Signed-off-by: wangqiang16 <wangqiang16@lenovo.com>

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello start your journey. </Text>
+        <Text>Changes you make will automatically reload. emulator</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
